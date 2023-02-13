99d2f5527ca805ea8782e065a89726ef6e26e264
Liviu Deleu <liviu.deleu.2002@gmail.com>
2023-02-13 16:16:58 +0200

    Homework1

---
+++ 3.3.2. Practical tasks for JavaScriptfundamentals/1/aplication.js
@@ -0,0 +1,48 @@
+let n = 9;
+if(n < 0 ){
+    console.log("This number not exist!");
+} else if( n > 9){
+    console.log("This number not exist!");
+}
+switch (n){
+    case 0:
+      if(n === 0){
+        console.log("zero!")
+      }
+    case 1:
+        if(n === 1){
+          console.log("one!")
+        }  
+    case 2:
+      if(n === 2){
+        console.log("two!")
+      }
+    case 3:
+        if(n === 3){
+          console.log("three!")
+        }
+    case 4:
+        if(n === 4){
+        console.log("three!")
+      }
+    case 5:
+        if(n === 5){
+          console.log("five!")
+        }
+    case 6:
+      if(n === 6){
+        console.log("six!")
+      }
+    case 7:
+        if(n === 7){
+          console.log("seven!")
+        }
+    case 8:
+            if(n === 8){
+              console.log("eight!")
+            }
+    case 9:
+      if(n === 9){
+        console.log("nine!")
+      }
+  }
---
+++ Tasks.md
@@ -0,0 +1,1 @@
+https://docs.google.com/document/d/1hpY4Eey9gCDLIukp1r1ZECVU_sy6gizrvvb4UcPp5N8/edit?usp=sharing
