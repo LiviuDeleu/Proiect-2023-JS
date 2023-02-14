6d6abe959e42adc4d2d30e6c0a13682d1b9f7f12
Liviu Deleu <liviu.deleu.2002@gmail.com>
2023-02-14 11:50:57 +0200

    Task2

---
+++ 3.3.2. Practical tasks for JavaScriptfundamentals/2/app.js
@@ -0,0 +1,10 @@
+let a = 1; 
+let b = 10; 
+let sum = 0; 
+
+for (let i = a; i <= b; i++) {
+  if (i % 2 === 0) {
+    sum += i; 
+  }
+}
+console.log(`Suma numerelor pare din intervalul [${a}, ${b}] este : ${sum}`);
