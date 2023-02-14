5a7db971859788f76470714481ff2485f1195a49
Liviu Deleu <liviu.deleu.2002@gmail.com>
2023-02-14 11:57:44 +0200

    Task3

--- 3.3.2. Practical tasks for JavaScriptfundamentals/2/app.js
+++ 3.3.2. Practical tasks for JavaScriptfundamentals/2/app.js
@@ -1,7 +1,6 @@
 let a = 1; 
 let b = 10; 
 let sum = 0; 
-
 for (let i = a; i <= b; i++) {
   if (i % 2 === 0) {
     sum += i; 
---
+++ 3.3.2. Practical tasks for JavaScriptfundamentals/3/app.js
@@ -0,0 +1,4 @@
+let a = 12;
+let b = 7;
+let c = 2;
+console.log(`Cel mai mic numar: ${Math.min(a, b, c)}, cel mai mare numar: ${Math.max(a, b, c)}.`);
